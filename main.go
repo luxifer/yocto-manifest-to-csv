4f2b2ebdb26e49acf1319bcb076ff12025a99793
Florent Viel <fvi@cisco.com>
2021-03-09 10:39:51 +0100

    Initial commit

---
+++ main.go
@@ -0,0 +1,79 @@
+package main
+
+import (
+	"bufio"
+	"encoding/csv"
+	"flag"
+	"fmt"
+	"os"
+	"strings"
+)
+
+var inputFile string
+var outputFile string
+
+func init() {
+	flag.StringVar(&inputFile, "input", "", "path of the yocto file to parse")
+	flag.StringVar(&outputFile, "output", "", "path of the CSV file to write, default to stdout")
+}
+
+func main() {
+	flag.Parse()
+
+	fi, err := os.Open(inputFile)
+	if err != nil {
+		printErr("Unable to open file: %s", err)
+		os.Exit(1)
+	}
+
+	defer fi.Close()
+
+	s := bufio.NewScanner(fi)
+
+	var list [][]string
+	lastItem := make([]string, 0)
+	header := make([]string, 0)
+	var headerDone bool
+
+	for s.Scan() {
+		line := s.Text()
+
+		if line == "" {
+			list = append(list, lastItem)
+			lastItem = make([]string, 0)
+			headerDone = true
+			continue
+		}
+
+		splits := strings.SplitN(line, ":", 2)
+		lastItem = append(lastItem, strings.TrimLeft(splits[1], " "))
+
+		if !headerDone {
+			header = append(header, splits[0])
+		}
+	}
+
+	fo := os.Stdout
+
+	if outputFile != "" {
+		fo, err = os.Open(outputFile)
+		if err != nil {
+			printErr("Unable to open file: %s", err)
+			os.Exit(1)
+		}
+		defer fo.Close()
+	}
+
+	w := csv.NewWriter(fo)
+	w.Write(header)
+
+	for _, v := range list {
+		w.Write(v)
+	}
+
+	w.Flush()
+}
+
+func printErr(format string, args ...interface{}) {
+	_, _ = fmt.Fprintf(os.Stderr, format, args...)
+}
